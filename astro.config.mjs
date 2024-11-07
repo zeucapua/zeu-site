75596624d9e1742e5d262a463df578330108205c
Zeu Capua <coding@zeu.dev>
2024-11-06 16:03:28 -0800

    replace vercel with netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), mdx(), icon()],
   output: "server",
-  adapter: vercel({ webAnalytics: { enabled: true }})
+  adapter: netlify(),
 });
---
+++ src/content/blog/atproto-oauth.md
@@ -0,0 +1,6 @@
+---
+title: AT Protocol OAuth Basics Tutorial
+description: Implement your own Bluesky client using the `@atproto/oauth-client-node` package
+date: "2024-10-29"
+draft: true
+---
